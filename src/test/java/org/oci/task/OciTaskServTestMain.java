e4a32364cd73c93464dc75e06696e08fc0d8bb88
Rajesh Raghavakurup <rajeshraghavakurup@Rajeshs-Air.attlocal.net>
2023-01-18 23:38:29 -0800

    adding functional test

---
+++ src/test/java/org/oci/task/OciTaskServTestMain.java
@@ -0,0 +1,17 @@
+package org.oci.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @brief Entry point for OCI Task Service Functional Tests
+ * @author rajeshkurup@live.com
+ */
+@SpringBootApplication
+public class OciTaskServTestMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OciTaskServTestMain.class, args);
+    }
+
+}
---
+++ src/test/java/org/oci/task/api/OciTaskApiFunctionalTest.java
@@ -0,0 +1,45 @@
+package org.oci.task.api;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.oci.task.OciTaskServTestMain;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @brief Functional tests for OCI Task Service APIs.
+ * @author rajeshkurup@live.com
+ */
+@SpringBootTest(classes = OciTaskServTestMain.class)
+public class OciTaskApiFunctionalTest {
+
+    private RequestSpecification httpRequest;
+
+    @BeforeEach
+    public void init() {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+        httpRequest = RestAssured.given();
+    }
+
+    @Test
+    public void testGetTasksSuccess() throws JSONException {
+        Response httpResp = httpRequest.request(Method.GET, "/tasks");
+
+        Assertions.assertEquals(200, httpResp.getStatusCode());
+
+        JSONObject apiResp = new JSONObject(httpResp.getBody().print());
+        JSONArray tasks = apiResp.getJSONArray("tasks");
+
+        Assertions.assertTrue(tasks.length() > 0);
+        Assertions.assertTrue(tasks.getJSONObject(0).getLong("id") > 0);
+        Assertions.assertTrue(tasks.getJSONObject(0).getString("title").length() > 0);
+    }
+
+}
