9ddb6bb81df297a356642b39d200915e34322d9e
Rajesh Raghavakurup <rajeshraghavakurup@Rajeshs-Air.attlocal.net>
2023-01-22 17:08:05 -0800

    adding functional tests

--- src/test/java/org/oci/task/api/OciTaskApiFunctionalTest.java
+++ src/test/java/org/oci/task/api/OciTaskApiFunctionalTest.java
@@ -9,7 +9,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,13 +23,10 @@ public class OciTaskApiFunctionalTest {
 
     private RequestSpecification httpRequest;
 
-    @BeforeEach
-    public void init() {
+    private JSONArray getTasks() throws JSONException {
         RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
         httpRequest = RestAssured.given();
-    }
 
-    private JSONArray getTasks() throws JSONException {
         Response httpResp = httpRequest.header("Accept", "application/json")
                 .request(Method.GET, "/tasks");
 
@@ -47,6 +43,9 @@ public class OciTaskApiFunctionalTest {
     }
 
     private long createTask() throws JSONException {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+        httpRequest = RestAssured.given();
+
         JSONObject task = new JSONObject();
         task.put("title", "Task from functional test");
 
@@ -67,6 +66,9 @@ public class OciTaskApiFunctionalTest {
     }
 
     private long updateTask(long taskId, JSONObject task) throws JSONException {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+        httpRequest = RestAssured.given();
+
         Response httpRespCreate = httpRequest.header("Content-Type", "application/json")
                 .header("Accept", "application/json")
                 .pathParam("id", taskId)
@@ -85,6 +87,9 @@ public class OciTaskApiFunctionalTest {
     }
 
     private JSONObject getTask(long taskId) throws JSONException {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+        httpRequest = RestAssured.given();
+
         Response httpRespGet = httpRequest.header("Accept", "application/json")
                 .pathParam("id", taskId)
                 .request(Method.GET, "/tasks/{id}");
@@ -101,6 +106,8 @@ public class OciTaskApiFunctionalTest {
     }
 
     private void deleteTasks(JSONArray tasks) throws JSONException {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+
         for(int i = 0; i < tasks.length(); i++) {
             if(StringUtils.equals(tasks.getJSONObject(i).getString("title"), "Task from functional test")) {
                 httpRequest = RestAssured.given();
@@ -115,6 +122,9 @@ public class OciTaskApiFunctionalTest {
 
     @Test
     public void testCreateAndGetTaskSuccess() throws JSONException {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+        httpRequest = RestAssured.given();
+
         long taskId = createTask();
 
         Assertions.assertTrue(taskId > 0L);
@@ -126,6 +136,9 @@ public class OciTaskApiFunctionalTest {
 
     @Test
     public void testGetTaskFailedTaskNotFound() throws JSONException {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+        httpRequest = RestAssured.given();
+
         Response httpRespGet = httpRequest.header("Accept", "application/json")
                 .pathParam("id", 0L)
                 .request(Method.GET, "/tasks/{id}");
@@ -135,6 +148,9 @@ public class OciTaskApiFunctionalTest {
 
     @Test
     public void testUpdateTasksSuccess() throws JSONException {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+        httpRequest = RestAssured.given();
+
         JSONArray tasks = getTasks();
 
         Assertions.assertNotNull(tasks);
@@ -151,6 +167,9 @@ public class OciTaskApiFunctionalTest {
 
     @Test
     public void testDeleteTasksSuccess() throws JSONException {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+        httpRequest = RestAssured.given();
+
         JSONArray tasks = getTasks();
 
         Assertions.assertNotNull(tasks);
@@ -160,11 +179,180 @@ public class OciTaskApiFunctionalTest {
 
     @Test
     public void testDeleteTasksFailedNoTaskFound() throws JSONException {
+        RestAssured.baseURI = "http://192.9.237.204:8081/v1/ocitaskserv";
+        httpRequest = RestAssured.given();
+
         Response httpRespGet = httpRequest.header("Accept", "application/json")
                 .pathParam("id", 0L)
                 .request(Method.DELETE, "/tasks/{id}");
 
         Assertions.assertEquals(400, httpRespGet.getStatusCode());
+    }
+
+    private JSONArray getTasksRest() throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+        httpRequest = RestAssured.given();
+
+        Response httpResp = httpRequest.header("Accept", "application/json")
+                .request(Method.GET, "/tasks");
+
+        Assertions.assertEquals(200, httpResp.getStatusCode());
+
+        JSONObject apiResp = new JSONObject(httpResp.getBody().print());
+        JSONArray tasks = apiResp.getJSONArray("tasks");
+
+        Assertions.assertTrue(tasks.length() > 0);
+        Assertions.assertTrue(tasks.getJSONObject(0).getLong("id") > 0L);
+        Assertions.assertTrue(tasks.getJSONObject(0).getString("title").length() > 0);
+
+        return tasks;
+    }
+
+    private long createTaskRest() throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+        httpRequest = RestAssured.given();
+
+        JSONObject task = new JSONObject();
+        task.put("title", "Task from functional test");
+
+        Response httpRespCreate = httpRequest.header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .body(task.toString())
+                .request(Method.POST, "/tasks");
+
+        Assertions.assertEquals(201, httpRespCreate.getStatusCode());
+
+        JSONObject apiRespCreate = new JSONObject(httpRespCreate.getBody().print());
+
+        long taskId = apiRespCreate.getLong("taskId");
+
+        Assertions.assertTrue(taskId > 0L);
+
+        return taskId;
+    }
+
+    private long updateTaskRest(long taskId, JSONObject task) throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+        httpRequest = RestAssured.given();
+
+        Response httpRespCreate = httpRequest.header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .pathParam("id", taskId)
+                .body(task.toString())
+                .request(Method.PUT, "/tasks/{id}");
+
+        Assertions.assertEquals(200, httpRespCreate.getStatusCode());
+
+        JSONObject apiRespCreate = new JSONObject(httpRespCreate.getBody().print());
+
+        long resultTaskId = apiRespCreate.getLong("taskId");
+
+        Assertions.assertTrue(resultTaskId > 0L);
+
+        return resultTaskId;
+    }
+
+    private JSONObject getTaskRest(long taskId) throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+        httpRequest = RestAssured.given();
+
+        Response httpRespGet = httpRequest.header("Accept", "application/json")
+                .pathParam("id", taskId)
+                .request(Method.GET, "/tasks/{id}");
+
+        Assertions.assertEquals(200, httpRespGet.getStatusCode());
+
+        JSONObject apiRespGet = new JSONObject(httpRespGet.getBody().print());
+        JSONObject taskReceived = apiRespGet.getJSONObject("task");
+
+        Assertions.assertEquals(taskId, taskReceived.getLong("id"));
+        Assertions.assertEquals("Task from functional test", taskReceived.getString("title"));
+
+        return taskReceived;
+    }
+
+    private void deleteTasksRest(JSONArray tasks) throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+
+        for(int i = 0; i < tasks.length(); i++) {
+            if(StringUtils.equals(tasks.getJSONObject(i).getString("title"), "Task from functional test")) {
+                httpRequest = RestAssured.given();
+                Response httpRespGet = httpRequest.header("Accept", "application/json")
+                        .pathParam("id", tasks.getJSONObject(i).getLong("id"))
+                        .request(Method.DELETE, "/tasks/{id}");
+
+                Assertions.assertEquals(200, httpRespGet.getStatusCode());
+            }
+        }
+    }
+
+    @Test
+    public void testCreateAndGetTaskSuccessRest() throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+        httpRequest = RestAssured.given();
+
+        long taskId = createTaskRest();
+
+        Assertions.assertTrue(taskId > 0L);
+
+        JSONObject taskReceived = getTaskRest(taskId);
+
+        Assertions.assertNotNull(taskReceived);
+    }
+
+    @Test
+    public void testGetTaskFailedTaskNotFoundRest() throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+        httpRequest = RestAssured.given();
+
+        Response httpRespGet = httpRequest.header("Accept", "application/json")
+                .pathParam("id", 0L)
+                .request(Method.GET, "/tasks/{id}");
+
+        Assertions.assertEquals(400, httpRespGet.getStatusCode());
+    }
+
+    @Test
+    public void testUpdateTasksSuccessRest() throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+        httpRequest = RestAssured.given();
+
+        JSONArray tasks = getTasksRest();
+
+        Assertions.assertNotNull(tasks);
+
+        JSONObject task = tasks.getJSONObject(0);
+
+        long taskId = task.getLong("id");
+        task.put("description", "Task from functional test - Description");
+
+        long resultTaskId = updateTaskRest(taskId, task);
+
+        Assertions.assertTrue(resultTaskId > 0L);
+    }
+
+    @Test
+    public void testDeleteTasksSuccessRest() throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+        httpRequest = RestAssured.given();
+
+        JSONArray tasks = getTasksRest();
+
+        Assertions.assertNotNull(tasks);
+
+        deleteTasksRest(tasks);
+    }
+
+    @Test
+    public void testDeleteTasksFailedNoTaskFoundRest() throws JSONException {
+        RestAssured.baseURI = "http://138.2.233.236:8080/application/v1/ocitaskrestservice";
+        httpRequest = RestAssured.given();
+
+        Response httpRespGet = httpRequest.header("Accept", "application/json")
+                .pathParam("id", 0L)
+                .request(Method.DELETE, "/tasks/{id}");
+
+        Assertions.assertEquals(200, httpRespGet.getStatusCode());
     }
 
 }
